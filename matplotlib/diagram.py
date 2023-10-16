358a5037c83dbb07b89178fae968ab9225f5e213
MakeIt2021 <boolean0707@gmail.com>
2023-10-16 22:46:26 +0300

    Update diagram.py

--- matplotlib/diagram.py
+++ matplotlib/diagram.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 from random import randint
 
 
-def diagram(n):
-    plt.title('Круговая диаграмма')
+def barer():
+    plt.title('Столбчатая диаграмма')
+    plt.ylabel('values')
+    plt.xlabel('fields')
     plt.grid()
 
+    fields = [0, 1, 2, 3, 4, 5, 6]
     values = []
-    for _ in range(n - 1):
-        val = randint(1, int(100 / n))
-        values.append(val)
-    values.append(100 - sum(values))
+    for _ in fields:
+        values.append(randint(1, 100))
+    plt.bar(fields, values)
 
-    plt.pie(values)
     plt.show()
 
-diagram(6)
+barer()
