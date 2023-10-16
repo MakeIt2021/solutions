77d0485190433936868e82aa59e2af6130e4c930
MakeIt2021 <boolean0707@gmail.com>
2023-10-16 22:01:20 +0300

    Create bar.py

---
+++ matplotlib/bar.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from random import randint
+
+plt.title('Столбчатая диаграмма')
+plt.ylabel('values')
+plt.xlabel('fields')
+plt.grid()
+
+fields = [0, 1, 2, 3, 4, 5, 6]
+values = []
+for _ in fields:
+    values.append(randint(1, 100))
+plt.bar(fields, values)
+
+plt.show()
